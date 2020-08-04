1ff2ac888dd803c31ca3b22be3f72f92fa5a0559
he44 <he44@illinois.edu>
2020-08-03 23:22:36 -0500

    copied more files from the aforementioend github repo, able to use one model. Next step: modifly the eval function to return the model somehow? Same code, same interpreter, won't run in VS code, but runs in pycharm

--- scratch/test.py
+++ scratch/test.py
@@ -1,6 +1,35 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from tensorflow.keras.models import load_model
 import tensorflow as tf
 print(tf.__version__)
 
+"""
 model = load_model('EfficientNet_B0.h5')
 print(model.summary())
+"""
+
+"""
+model_name = 'efficientnet-b0'
+ckpt_dir = model_name
+checkpoint = tf.train.latest_checkpoint(ckpt_dir)
+print(type(checkpoint), checkpoint)
+"""
+
+# Following this notebook: https://github.com/tensorflow/tpu/blob/master/models/official/efficientnet/eval_ckpt_example.ipynb
+import eval_ckpt_main as eval_ckpt
+model_name = 'efficientnet-b0'
+ckpt_dir = model_name
+eval_driver = eval_ckpt.get_eval_driver(model_name)
+print(type(eval_driver))
+
+image_file = 'panda.jpg'
+image_files = [image_file]
+
+labels_map_file = 'labels_map.json'
+
+pred_idx, pred_prob = eval_driver.eval_example_images(
+    ckpt_dir, image_files, labels_map_file
+)
+
+print(pred_idx, pred_prob)
